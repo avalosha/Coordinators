adea58f7058950298a3011e8a6b894aa78d719fb
Esfera Estudios <github@sferea.com>
2024-02-14 15:26:15 -0600

    Añadir TabBarController.

--- Coordinators/Coordinators/SceneDelegate.swift
+++ Coordinators/Coordinators/SceneDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var coordinator: MainCoordinator?
     var window: UIWindow?
 
 
@@ -18,13 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        let navController = UINavigationController()
-        coordinator = MainCoordinator(navigationController: navController)
-        coordinator?.start()
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navController
+        window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
     }
 
