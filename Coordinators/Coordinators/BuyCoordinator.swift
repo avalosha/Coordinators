3a116862e23f784d4e22a0cd750a0adfc67ff17c
Esfera Estudios <github@sferea.com>
2024-02-14 14:15:29 -0600

    Añadir coordinadores secundarios con devoluciones sencillas.

---
+++ Coordinators/Coordinators/BuyCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  BuyCoordinator.swift
+//  Coordinators
+//
+//  Created by Sferea-Lider on 14/02/24.
+//
+
+import UIKit
+
+class BuyCoordinator: Coordinator {
+    weak var parentCoordinator: MainCoordinator?
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func didFinishBuying() {
+        parentCoordinator?.childDidFinish(self)
+    }
+}
